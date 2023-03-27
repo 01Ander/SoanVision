d5947bbbbad7656742ac1aac16731d3925794391
01Ander <arinconayala@hotmail.com>
2023-03-27 16:30:14 -0500

    add fonts, and header style

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import '../src/sass/reset.sass';
-import '../src/sass/_var.sass'
+// import '../src/sass/_var.sass'
 import '../src/sass/header.sass';
 import logo from "../src/img/Logo.svg";
+
 
 const logoHeader = document.querySelector('.header__logo');
 logoHeader.style.background = `url(${logo}) center no-repeat`;
