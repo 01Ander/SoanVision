2bf51af2a087702ecc288d9cca2565795272e6d0
01Ander <arinconayala@hotmail.com>
2023-03-27 17:53:49 -0500

    edit

--- src/index.js
+++ src/index.js
@@ -3,6 +3,5 @@ import '../src/sass/_style.sass';
 import logo from "../src/img/Logo.svg";
 import icon from "../src/img/lupa.svg";
 
-
 logoHeader.style.background = `url(${logo}) center no-repeat`;
 imgSearch.style.background = `url(${icon}) center no-repeat`;
