83979d973b72f27d6cc8522c75ea277ab53fe0c4
01Ander <arinconayala@hotmail.com>
2023-03-28 12:09:13 -0500

    ready gallery principal style

--- src/index.js
+++ src/index.js
@@ -5,10 +5,14 @@ import icon from "../src/img/lupa.svg";
 import { random } from '../src/js/_getData.js'
 
 
-// const API_KEY = process.env.API_KEY;
-
 logoHeader.style.background = `url(${logo}) center no-repeat`;
 imgSearch.style.background = `url(${icon}) center no-repeat`;
 
 
-random();
+function initApp() {
+  // random();
+  console.log('ready');
+}
+
+
+document.addEventListener('DOMContentLoaded', initApp);
--- src/js/_getData.js
+++ src/js/_getData.js
@@ -1,11 +1,12 @@
 import axios from 'axios';
 
 const unsplashAccessKey = process.env.UNSPLASH_ACCESS_KEY;
+const API_KEY = process.env.API_KEY;
 
 const api = axios.create({
-  baseURL: 'https://api.unsplash.com/',
+  baseURL: 'https://api.pexels.com/v1/',
   headers: {
-    Authorization: `Client-ID ${unsplashAccessKey}`,
+    Authorization: API_KEY,
   },
 });
 
@@ -13,17 +14,21 @@ const galleryContainer = document.querySelector('.gallery__container');
 
 export function random() {
   api
-    .get('/photos/random')
+    .get('search?query=nature&per_page=10&page=1')
     .then(res => {
-      console.log(res.data);
+      // console.log(res.data);
+      // console.log(res.data.photos);
+      const photos = res.data.photos;
+      console.log(photos);
       const toRender = [];
 
-      res.forEach(element => {
+      photos.forEach(element => {
         const article = document.createElement('div');
         const img = document.createElement('img');
 
         article.classList.add('gallery__image-container');
         img.classList.add('gallery__image');
+        img.src = element.src.original
 
         article.appendChild(img)
 
@@ -36,6 +41,7 @@ export function random() {
     });
 
 }
+
 
 
 
